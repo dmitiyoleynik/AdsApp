3af3a94b0c5c598252209446d81bdce8c3282b3c
Dmytro <dmitiyoleynik@gmail.com>
2021-01-20 22:25:49 +0200

    Add DB context

---
+++ AdsApp/DAL/ApplicationDBContext.cs
@@ -0,0 +1,17 @@
+﻿using DAL.EFModels;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ApplicationDBContext :DbContext
+    {
+        public DbSet<Ad> Ads { get; set; }
+        public ApplicationDBContext (DbContextOptions<ApplicationDBContext> options)
+            : base(options)
+        { }
+    }
+}
